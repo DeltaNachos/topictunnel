a9c1202d736ea527df5254e68aa6ac834c0a41ad
DeltaNachos <dragix2@gmail.com>
2023-11-12 00:45:00 -0600

    Added HTTP POST and event listener

--- background.js
+++ background.js
@@ -1,55 +1,6 @@
-let timerInterval; // tick length
-let timeLeft; // time left on work or break timer
-let isRunning = false; // is the timer running
-
-async function init() { // start the timer
-    let i = 0;
-    while (i < 3) { // traditional pomodoros run 4 times
-        const [workDuration, breakDuration] = await Promise.all([
-        chrome.storage.local.get(['pomodoroWork']),
-        chrome.storage.local.get(['pomodoroBreak'])
-        ]); // gets current timer settings from options
-
-        timeLeft = workDuration.pomodoroWork || 1500; // Default to 25 minutes
-        setTimeout(() => {
-            timeLeft = breakDuration.pomodoroBreak || 300; // Default to 5 minutes
-        }, timeLeft * 1000);
-        i++; 
-    }
-}
-
-function tick() {
-  timeLeft -= 1; // decrease time by 1 sec
-  if (timeLeft <= 0) { // timer expires
-    clearInterval(timerInterval);
-    chrome.tabs.query({}, (tabs) => { // gets every tab
-      tabs.forEach((tab) => {
-        chrome.tabs.sendMessage(tab.id, { message: "done" }); // timer is done
-      });
-    });
-  } else {
-    chrome.tabs.query({}, (tabs) => {
-      tabs.forEach((tab) => {
-        chrome.tabs.sendMessage(tab.id, { message: "tick" }); // timer counts down 1
-      });
-    });
-  }
-}
-
-chrome.runtime.onMessage.addListener((message) => {
-  if (message == 'startTimer') { // trigger to start
-    if (!isRunning) {
-        isRunning = true; // timer starts
-        timerInterval = setInterval(tick(), 1000); // define tick as 1 second
-    }
-  }
-});
-
-init(); 
-
 // get tab title on change
-
 var activeTabTitle;
+var lastTabTitle;
 
 // Function to get the title of the active tab
 function getActiveTabTitle() {
@@ -58,13 +9,45 @@ function getActiveTabTitle() {
         const activeTabId = tabs[0].id;
         chrome.tabs.get(activeTabId, function(tab) {
           activeTabTitle = tab.title;
+          if (activeTabTitle == lastTabTitle) {return;}
+          lastTabTitle = activeTabTitle;
+          fetcher();
+          //console.log(activeTabTitle);
+          //return activeTabTitle;
           // You can use activeTabTitle for your operations
         });
       }
     });
   }
-  
+
   // Event listener for tab activation changes
   chrome.tabs.onActivated.addListener(function(activeInfo) {
-    getActiveTabTitle(); // Retrieve the title whenever the active tab changes
+    getActiveTabTitle();
+     // Retrieve the title whenever the active tab changes
   });
+
+  // Event listener for tab name changes
+  chrome.tabs.onUpdated.addListener(function(activeInfo) {
+    getActiveTabTitle();
+    // Retrieve the title whenever a tab changes its name
+  });
+
+// HTTP POST to cloud compute
+function fetcher() {
+  fetch("https://us-central1-topictunnel.cloudfunctions.net/http-test", {
+    method: "POST",
+    body: JSON.stringify({
+      "prompt": "fortnite",
+      "text": activeTabTitle
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then(output => output.text())
+    .then((output) => {
+      console.log(output)
+      console.log(activeTabTitle)
+    })
+    .catch(err => console.log(err));
+}
